b1dcb35a141e46dd22a1d9d72ebddeae005e1ac2
Charlton Trezevant <ct@ctis.me>
2019-12-19 13:46:03 -0500

    Error message newlines

--- main.go
+++ main.go
@@ -15,25 +15,25 @@ func main() {
 
 	appCfg, err := config.Load(*configFile)
 	if err != nil {
-		fmt.Printf("Error reading configuration file: %s", err.Error())
+		fmt.Printf("Error reading configuration file: %s\n", err.Error())
 		os.Exit(1)
 	}
 
 	if err = hw.Init(); err != nil {
-		fmt.Printf("Error initializing GPIO: %s", err.Error())
+		fmt.Printf("Error initializing GPIO: %s\n", err.Error())
 		os.Exit(1)
 	}
 	defer func() {
 		err := hw.Close()
 		if err != nil {
-			fmt.Printf("Error un-initializing GPIO: %s", err.Error())
+			fmt.Printf("Error un-initializing GPIO: %s\n", err.Error())
 			os.Exit(1)
 		}
 	}()
 
 	apiServer := api.NewServer(appCfg)
 	if err = apiServer.Serve(); err != nil {
-		fmt.Printf("API server died with error: %s", err.Error())
+		fmt.Printf("API server died with error: %s\n", err.Error())
 		os.Exit(1)
 	}
 
