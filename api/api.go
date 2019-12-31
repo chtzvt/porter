c6659c2dca8e8530f1693a96b12ef0876d18a50c
Charlton Trezevant <ct@ctis.me>
2019-12-31 14:17:10 -0500

    Add API request logging

--- api/api.go
+++ api/api.go
@@ -39,16 +39,16 @@ func (s *Server) Serve() error {
 		mux.Handle("/", http.RedirectHandler("https://github.com/ctrezevant/porter", http.StatusFound))
 	}
 
-	mux.Handle("/api/v1/list", s.checkAuth(s.handleListDoors()))
+	mux.Handle("/api/v1/list", s.log(s.checkAuth(s.handleListDoors())))
 
-	mux.Handle("/api/v1/state/", s.checkAuth(s.handleDoorState()))
+	mux.Handle("/api/v1/state/", s.log(s.checkAuth(s.handleDoorState())))
 
-	mux.Handle("/api/v1/lock/", s.checkAuth(s.handleDoorLock()))
-	mux.Handle("/api/v1/unlock/", s.checkAuth(s.handleDoorUnlock()))
+	mux.Handle("/api/v1/lock/", s.log(s.checkAuth(s.handleDoorLock())))
+	mux.Handle("/api/v1/unlock/", s.log(s.checkAuth(s.handleDoorUnlock())))
 
-	mux.Handle("/api/v1/open/", s.checkAuth(s.handleDoorOpen()))
-	mux.Handle("/api/v1/close/", s.checkAuth(s.handleDoorClose()))
-	mux.Handle("/api/v1/trip/", s.checkAuth(s.handleDoorTrip()))
+	mux.Handle("/api/v1/open/", s.log(s.checkAuth(s.handleDoorOpen())))
+	mux.Handle("/api/v1/close/", s.log(s.checkAuth(s.handleDoorClose())))
+	mux.Handle("/api/v1/trip/", s.log(s.checkAuth(s.handleDoorTrip())))
 
 	if s.Config.HTTP.TLSCertFile != "" && s.Config.HTTP.TLSKeyFile != "" {
 		return http.ListenAndServeTLS(s.Config.HTTP.ListenAddr, s.Config.HTTP.TLSCertFile, s.Config.HTTP.TLSKeyFile, mux)
